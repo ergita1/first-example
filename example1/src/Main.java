4690eefbb9425968a61776ddf38930a9deea431a
ergita1 <ecurri21@epoka.edu.al>
2023-12-09 11:43:40 +0100

    hi!

--- example1/src/Main.java
+++ example1/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
    .
 
-        System.out.println("hey there");
+        System.out.println("hi!");
         System.out.printf("Hello!");
 
         
