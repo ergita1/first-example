726d4e6e2f296b7d92a5eea25b287cb748c3040c
ergita1 <ecurri21@epoka.edu.al>
2023-12-09 11:19:48 +0100

    insert sout

--- example1/src/Main.java
+++ example1/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
+
+        System.out.println("hey there");
         System.out.printf("Hello and welcome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
